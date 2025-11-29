618b3d7276866aadc3291a2f5f4f6df184b70791
Onimisi Adelza Adeolu <onimisiadeolu@gmail.com>
2025-11-29 13:30:55 +0100

    casees

--- py.py
+++ py.py
@@ -3,5 +3,10 @@
 # height = 5.9
 
 # print('hello world !', type(height))
-sentence = 'This is a sample sentence.'
-print(len(sentence), sentence[0])
+# sentence = 'This is a sample sentence.'
+# print(len(sentence), sentence[0])
+#  learning upper cases and lower cases
+my_str = 'hello world'
+to_upper = my_str.upper()
+to_lower = my_str.lower()
+print(to_upper, to_lower)
