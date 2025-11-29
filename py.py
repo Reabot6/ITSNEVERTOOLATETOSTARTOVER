75d48abef7dd33926fc6a3fce9dd131efb28fedc
Onimisi Adelza Adeolu <onimisiadeolu@gmail.com>
2025-11-29 13:26:23 +0100

    basic syntaxes

--- py.py
+++ py.py
@@ -1,5 +1,7 @@
 # name = 'john Doe'
 # age = 30
-height = 5.9
+# height = 5.9
 
-print('hello world !', type(height))
+# print('hello world !', type(height))
+sentence = 'This is a sample sentence.'
+print(len(sentence), sentence[0])
